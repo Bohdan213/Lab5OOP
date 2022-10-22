715d8ce66ba7465d49c273a879f5aa330fd8226f
Bohdan213 <bogdanpeleh213@gmail.com>
2022-10-22 18:09:56 +0300

    checkstyle.zml upgraded

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -3,18 +3,35 @@ package flower.store;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 
 
 @Setter
 public class Flower {
     @Getter
+    @Setter
     private double sepalLength;
+    @Setter
     private FlowerColor color;
     @Getter
+    @Setter
     private double price;
     @Getter
+    @Setter
     private FlowerType flowerType;
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    //    @Getter
+//    private T1 sepalLength;
+//    private T2 color;
+//    @Getter
+//    private T3 price;
+//    @Getter
+//    private T4 flowerType;
     public String getColor() {
         return color.toString();
     }
